80acefbc5cb78fe91c3064ec639be0b5f6e87644
PivovarovIV <brauer.igor@gmail.com>
2023-11-29 23:57:00 +0300

    Drawing timer view

---
+++ Scrumdinger/SpeakerArc.swift
@@ -0,0 +1,32 @@
+//
+//  SpeakerArc.swift
+//  Scrumdinger
+//
+//  Created by Игорь Пивоваров on 28.11.2023.
+//
+
+import SwiftUI
+
+struct SpeakerArc: Shape {
+    let speakerIndex: Int
+    let totalSpeakers: Int
+    
+    private var degreesPerSpeakers: Double {
+        365.0 / Double(totalSpeakers)
+    }
+    private var startAngle: Angle {
+        Angle(degrees: degreesPerSpeakers * Double(speakerIndex) + 1.0)
+    }
+    private var endAngle: Angle {
+        Angle(degrees: startAngle.degrees + degreesPerSpeakers - 1.0)
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        let diameter = min(rect.size.width, rect.size.height) - 24.0
+        let radius = diameter / 2.0
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        return Path { path in
+            path.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+        }
+    }
+}
